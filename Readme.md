45dc512450017660b84f1a764a2c3da57c7efc60
Dev Expert <DeveExpert1879@gmail.com>
2023-07-23 13:42:49 -0500

    updatd with folders

--- Readme.md
+++ Readme.md
@@ -1,4 +1,7 @@
-curl -X POST -F "file=@D:/1/work/SnapLync/whatsapp.mp3" http://127.0.0.1:8000/upload/audio/ 
+curl -X POST -F "file=@D:/1/work/SnapLync/whatsapp.mp3" http://67.205.185.110:8080/upload/audio/ 
+
+uvicorn main:app --host 0.0.0.0 --port 8080
 
 123qwe!@#QWE
 
+67.205.185.110
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import json
 import wave
 from vosk import Model, KaldiRecognizer, SetLogLevel
 import time
+import os
+import datetime
 
 
 # available models = ['tiny.en', 'tiny', 'base.en', 'base', 'small.en', 'small', 'medium.en', 'medium', 'large-v1', 'large-v2', 'large']
@@ -18,6 +20,9 @@ model_path = "vosk-model-en-us-0.22"
 model_vosk = Model(model_path) 
 filter_words = ['well','oh', 'um' 'er' ,'ah', 'uh', 'hmm', 'like', 'actually', 'basically', 'seriously', 'literally', 'totally', 'clearly', 'you see', 'you know'
     , 'i mean', 'you know what I mean','yeah', 'at the end of the day', 'believe me', 'i guess' , 'i suppose', 'or something', 'okay' , 'so', 'right' , 'hmm' , 'uh' ,'huh']
+
+
+print('Server is started ...')
 
 app = FastAPI()
 
@@ -48,16 +53,16 @@ class AudioProcessResult(BaseModel):
     filename: str
     duration_seconds: float  # You can add more fields depending on the processing you want to do
 
-def process_audio(file_path: str):
+def process_audio(folder_path: str, file_name: str):
     
-    start_ = time.time()
+    start_ = time.time()    
 
-    fname = file_path.split('.')[0]
+    fname = file_name.split('.')[0]
     # print(fname)
-    src = file_path
-    dst = file_path#.replace('mp3', 'wav')
-    # sound = AudioSegment.from_mp3(src)
-    # sound.export(dst, format="wav")
+    src = folder_path + "/" + file_name
+    dst = folder_path + "/" + file_name.replace('m4a', 'wav')
+    sound = AudioSegment.from_file(src)
+    sound.export(dst, format="wav")
 
     # Load the original audio file
     audio, original_sample_rate = sf.read(dst)
@@ -72,14 +77,14 @@ def process_audio(file_path: str):
     resampled_audio = scipy.signal.resample(audio, int(len(audio) * resampling_ratio))
 
     # Save the resampled audio to a new file
-    sf.write(fname + "_resampled.wav", resampled_audio, target_sample_rate)
+    sf.write(folder_path + "/" + fname + "_resampled.wav", resampled_audio, target_sample_rate)
 
 
-    sound = AudioSegment.from_wav(fname + "_resampled.wav")
+    sound = AudioSegment.from_wav(folder_path + "/" + fname + "_resampled.wav")
     sound = sound.set_channels(1)
-    sound.export(fname + "_resampled_mono.wav", format="wav")
+    sound.export(folder_path + "/" + fname + "_resampled_mono.wav", format="wav")
 
-    result = model.transcribe(fname + "_resampled_mono.wav")
+    result = model.transcribe(folder_path + "/" + fname + "_resampled_mono.wav")
     # print(result['text'])
     sentences = []
     json_sentence = []
@@ -103,13 +108,13 @@ def process_audio(file_path: str):
     
     # json_sentence_data
     json_sentence_data = json.dumps(json_sentence)
-    with open('sentence_json_data.json', 'w') as outfile:
+    with open(folder_path + "/" +'sentence_json_data.json', 'w') as outfile:
        outfile.write(json_sentence_data)
 
     
     # Part for using VOSK model
      
-    audio_filename = fname + "_resampled_mono.wav"
+    audio_filename = folder_path + "/" + fname + "_resampled_mono.wav"
 
 
     wf = wave.open(audio_filename, "rb")
@@ -163,7 +168,7 @@ def process_audio(file_path: str):
     print("The number of words is : ", len(json_words))
 
     json_words_data = json.dumps(json_words)
-    with open('words_json_data_final.json', 'w') as outfile:
+    with open(folder_path + "/" + 'words_json_data_final.json', 'w') as outfile:
         outfile.write(json_words_data)
 
     pause = []
@@ -178,7 +183,7 @@ def process_audio(file_path: str):
     
 
     json_pause_data = json.dumps(pause)
-    with open('pause_json_data_Gujrati_en.json', 'w') as outfile:
+    with open(folder_path + "/" + 'pause_json_data_Gujrati_en.json', 'w') as outfile:
         outfile.write(json_pause_data)
 
     
@@ -195,22 +200,36 @@ def process_audio(file_path: str):
     
 
     json_filtered_data = json.dumps(filtered_words)
-    with open('filtered_json_data_Gujrati_en.json', 'w') as outfile:
+    with open(folder_path + "/" + 'filtered_json_data_Gujrati_en.json', 'w') as outfile:
         outfile.write(json_filtered_data)
 
 
     end_ = time.time() - start_
     print("--- %s seconds ---" % end_)
+    
     return json_sentence_data, json_words, json_pause_data, json_filtered_data, end_
 
 @app.post("/upload/audio/")
 async def upload_audio_file(file: UploadFile = File(...)):
+
+    c_directory = os.getcwd()
+    
+    c_year = str(datetime.date.today().year)
+    c_date = str(datetime.date.today().month) + '-' + str(datetime.date.today().day)
+    
+    folder_name = c_directory + f"/data/{c_year}/{c_date}/{file.filename.split('.')[0]}"
+    fname = folder_name + f"/{file.filename}"
+
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
     # Save the uploaded audio file to a temporary location
-    with open(file.filename, "wb") as buffer:
-        buffer.write(await file.read())
+
+    with open(fname, "wb") as buffer:
+        buffer.write(await file.read())    
 
     # Process the uploaded audio file
-    sentence_json = process_audio(file.filename)
+    sentence_json = process_audio(folder_name, file.filename)
+    
 
     # Return the processing result
     # return AudioProcessResult(filename=file.filename, duration_seconds=duration_seconds)
